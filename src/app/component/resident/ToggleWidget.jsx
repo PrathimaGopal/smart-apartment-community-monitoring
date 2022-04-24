89666c52138003d3863b721b78054cbad68467f3
PrathimaGopal <gopalprathi@gmail.com>
2022-04-24 04:50:41 -0700

    add-resident-dashboards

---
+++ src/app/component/resident/ToggleWidget.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Switch from "@mui/material/Switch";
+
+function ToggleWidget() {
+  return (
+    <div>
+      <Switch></Switch>
+    </div>
+  );
+}
+
+export default ToggleWidget;
